8b00b27ffa2cc45596d9601f84117a11c499d322
Ana-ana <annapeshkova2013@gmail.com>
2020-07-24 14:52:22 +0300

    added is_prime func

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -3,21 +3,24 @@ from random import randint
 
 def run():
     question = randint(1, 11)
-    result = ''
-    if question > 1:
-        if question == 2 or question == 3:
-            result = "yes"
-        else:
-            for i in range(2, question):
-                if(question % i) == 0:
-                    result = 'no'
-                    break
-                else:
-                    result = 'yes'
-    else:
-        result = 'no'
+    result = is_prime(question)
     return str(question), result
 
 
 def DESCRIPTION():
     return 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+
+def is_prime(number):
+    if number > 1:
+        if number == 2 or number == 3:
+            return 'yes'
+        else:
+            for i in range(2, number):
+                if(number % i) == 0:
+                    return 'no'
+                    break
+                else:
+                    return 'yes'
+    else:
+        return 'no'
